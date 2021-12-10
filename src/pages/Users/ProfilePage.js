0168e99c0596cd45eec83f58b24aaa76041d15a0
aniljogireddy <anil.jogireddy@gmail.com>
2021-12-10 10:36:59 +0100

    profile user and edit profile user

---
+++ src/pages/Users/ProfilePage.js
@@ -0,0 +1,16 @@
+import SearchBar from '../../components/SearchBar'
+import {useState} from "react";
+import axios from 'axios';
+
+const API_URI = process.env.REACT_APP_API_URI;
+
+function ProfilePage(props) {
+
+return (
+    <div>
+        
+    </div>
+)
+}
+
+export default ProfilePage;
