92f9b01ee0c79884ccd8f1cc65f4b95d52271953
Amy Davies <amyloudav@gmail.com>
2021-12-10 17:57:56 +0100

    query is done

---
+++ src/components/ReservationCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ReservationCard(props) {
+    const { reservation } = props;
+
+    return (
+    <div className="reservation-card">
+        <p>Date: {reservation.day}</p>
+        <p>Time: {reservation.hour}</p>
+        <p>Guests: {reservation.people}</p>
+        <p>Status: {reservation.status}</p>
+    </div>
+    )
+}
---
+++ src/pages/ReservationListPage.js
@@ -0,0 +1,32 @@
+
+import { useState, useEffect } from 'react';
+import {useParams } from 'react-router-dom';
+import axios from 'axios';
+
+import React from 'react';
+import ReservationCard from '../components/ReservationCard';
+
+export default function ReservationListPage() {
+
+    const [reservations, setReservations] = useState({});
+    const { id: resId } = useParams()
+
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:5005/business/${resId}/reservations`)
+            .then((response) => {
+                setReservations(response.data.data)
+            })
+    }, [resId] );
+
+return (
+    <div>
+        <h1>Your Reservations</h1>
+        {reservations.map(reservation => {
+            return <ReservationCard key={reservation._id} reservation={reservation} />
+        }
+        )}
+    </div>
+)
+}
