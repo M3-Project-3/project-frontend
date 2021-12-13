477b0729c32457fa4a23a4f3db2e244c83ae8e03
Amy Davies <amyloudav@gmail.com>
2021-12-13 12:09:57 +0100

    class name edit

--- src/components/ReservationCard.js
+++ src/components/ReservationCard.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function ReservationCard(props) {
     const { reservation } = props;
-    console.log('dssdasddassda',reservation.date)
+
     return (
-    <div className="reservation-card">
+    <div className="reservationCard__container">
         <p>Date: {reservation.date}</p>
         <p>Time: {reservation.hour}</p>
         <p>Guests: {reservation.people}</p>
