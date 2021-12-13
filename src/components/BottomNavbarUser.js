e0e40e03a6ac505b911d9acbf49b2a470828a8ff
Amy Davies <amyloudav@gmail.com>
2021-12-13 18:43:34 +0100

    css changes

--- src/components/BottomNavbarUser.js
+++ src/components/BottomNavbarUser.js
@@ -19,36 +19,31 @@ export default function BottomNavbarUser(props){
 
             {isLoading === false && isLoggedIn ? (
             
-            <div className="bottomNavbar__container" >
-            
             <div className="bottomNavbar__textIconContainer">
-            <Link to="/">
-                    <img className="bottomNavbar__icons" src="magnifyingglassorange.png" />
-                    <p className="bottomNavbar__text" >Search</p>
+            
+            <div className="bottomNavbar__iconTextContainer">
+                <Link to="/">
+                    <img className="bottomNavbar__icon" src="magnifyingGlass.png"/>
+                    <p className="bottomNavbar__text">Search</p>
+                </Link>
+            </div>
+        
+            <Link to={`/${user._id}/reservations`}>   
+                <p className="bottomNavbar__text">Reservations</p>
             </Link>
-            </div>
 
-                <Link to={`/${user._id}/reservations`}>   
-                    <p className="bottomNavbar__text">Reservations</p>
-                </Link>
+            <Link to={`/${user._id}/favourites`}>
+                <p className="bottomNavbar__text">Favourites</p>
+            </Link>
 
-                <Link to={`/${user._id}/favourites`}>
-                    <p className="bottomNavbar__text">Favourites</p>
-                </Link>
-
-                <Link to={`/${user._id}`}>
-                 
-                    <p className="bottomNavbar__text">Profile</p>
-                </Link>
+            <Link to={`/${user._id}`}>
+                <p className="bottomNavbar__text">Profile</p>
+            </Link>
 
             </div>
 
-            ) : (
-                    <>
-                    <p>Created by Ironhackers</p>
-                 </>
-            )}
-
+            ) : (<p>Created by Ironhackers</p>)
+            }
         </div>
     )
 }
