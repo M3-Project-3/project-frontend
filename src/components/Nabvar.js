64443d66906daf9c98fae9470dc53a4ba0bcf67a
Amy Davies <amyloudav@gmail.com>
2021-12-15 16:42:10 +0100

    logo is a link

--- src/components/Nabvar.js
+++ src/components/Nabvar.js
@@ -1,43 +1,23 @@
 import { useContext } from "react"; // <== IMPORT
 import { AuthContext } from "../context/auth.context"; // <== IMPORT
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
     // Subscribe to the AuthContext to gain access to
     // the values from AuthContext.Provider `value` prop
-    const {
-        isLoading,
-        logOutUser,
-       isLoggedIn,
-       logOutBusiness,
-        businessIsLoggedIn,
-        businessIsLoading,
-    } = useContext(AuthContext);
+    const {} = useContext(AuthContext);
   
     return (
         
       <nav>
-        <img className="navBar__logo" src="/Mesa-logos_transparent.png">
-
-        </img>
+        <NavLink to="/" className="navBar__logo">
+          <a href="" className="navBar__logoLink" />
+            <img className="navBar__img" src="/logo.png" alt="">
+            </img>
+          </NavLink>
         </nav>
-      
-      /* {(isLoading === false && isLoggedIn) || (businessIsLoading === false && businessIsLoggedIn) ? (
-        <>
-          <button onClick={logOutUser || logOutBusiness}>Logout</button>
-       
   
-        </>
-      ) : (
-        <>
-          <Link to="/login" className="navBar__logInButton">
-            {" "}
-            <Link>Login</Link>{" "}
-          </Link>
-        </>
-      )} */
-   
-     
     );
   }
   
