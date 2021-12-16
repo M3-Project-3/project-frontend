9fd98e430af14bba63df81c3dcf1b2ffef8299ab
Carolina Romanos <carolina.romanos.roca@gmail.com>
2021-12-16 17:44:55 +0100

    fixes

--- src/components/Nabvar.js
+++ src/components/Nabvar.js
@@ -1,19 +1,16 @@
-import { useContext } from "react"; // <== IMPORT
-import { AuthContext } from "../context/auth.context";
-import { NavLink } from "react-router-dom";
+
+
 
 const Navbar = () => {
-    const {
-    } = useContext(AuthContext);
+   
   
     return (
         
       <nav>
-        <NavLink to="/" className="navBar__logo">
-          <a href="" className="navBar__logoLink" />
-            <img className="navBar__img" src="/logo.png" alt="">
-            </img>
-          </NavLink>
+      
+            <img className="navBar__img" src="/logo.png" alt=""/>
+
+
         </nav>
     );
   }
